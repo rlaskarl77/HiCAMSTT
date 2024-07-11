5af2c51f85aece937a51fc0fcf8f38d94b612957
yuna-102 <aldlt1226@gmail.com>
2024-07-11 14:14:43 +0900

    fix annotation.py

--- WorldTrack/utils/annotation.py
+++ WorldTrack/utils/annotation.py
@@ -22,5 +22,20 @@ class Camera:
 
 @dataclass
 class Data:
-    time: int
+    time: str
     camera: List[Camera]
+
+    def to_json(self) -> str:
+        return json.dumps(asdict(self), indent=4)
+
+    def save_to_file(self, filename: str) -> None:
+        with open(filename, 'w') as file:
+            json.dump(asdict(self), file, indent=4)
+
+    @staticmethod
+    def from_json(json_str: str) -> 'Data':
+        data_dict = json.loads(json_str)
+        for camera in data_dict['camera']:
+            camera['object_type'] = [ObjectType(**obj) for obj in camera['object_type']]
+        data_dict['camera'] = [Camera(**camera) for camera in data_dict['camera']]
+        return Data(**data_dict)
