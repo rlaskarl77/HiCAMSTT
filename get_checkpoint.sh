9da1d8c8549d145bd36788c4eea547e732f65bfa
Nam <rlaskarl77@snu.ac.kr>
2024-07-15 21:04:40 +0900

    added stream inference params

---
+++ get_checkpoint.sh
@@ -0,0 +1,2 @@
+mkdir -p ./checkpioints
+curl "https://drive.usercontent.google.com/download?id=1joi_iKsRxciP5jX8BS4WtF1AknkIdERK&confirm=xxx" -o ./checkpoints/model.ckpt
---
+++ run_rtsp.sh
@@ -0,0 +1,2 @@
+cd WorldTrack
+python inference_stream.py predict -c configs/d_stream_hdc.yml -c configs/m_mvdet.yml --ckpt ../checkpoints/model.ckpt
